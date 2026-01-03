00c7e3a4c67aa5d1035d275d2980184f1140aa52
Kirito-012 <11.uknowwho@gmail.com>
2026-01-03 15:15:59 +0530

    contact

---
+++ app/contact/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from 'react';
+import HeroSection from './HeroSection';
+
+const ContactPage = () => {
+    return (
+        <div className="min-h-screen w-full bg-black flex items-center justify-center py-12 px-4 sm:px-8">
+            <HeroSection />
+        </div>
+    );
+};
+
+export default ContactPage;
+
