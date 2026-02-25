dc916b45251dd767aef98237d11f375bfad2139b
Kirito-012 <11.uknowwho@gmail.com>
2026-02-25 20:56:54 +0530

    og-image added

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,31 @@ import { LoadingProvider } from './lib/LoadingContext';
 import AppWrapper from './Components/AppWrapper';
 
 export const metadata: Metadata = {
+	metadataBase: new URL('https://www.thecraftsync.com'),
 	title: 'TheCraftSync',
 	description: 'Digital Agency for all your crafting needs.Make your ideas come to life with our expert crafting services.',
+	openGraph: {
+		title: 'TheCraftSync | Digital Agency',
+		description: 'Digital Agency for all your crafting needs. Make your ideas come to life with our expert crafting services.',
+		url: 'https://www.thecraftsync.com',
+		siteName: 'TheCraftSync',
+		images: [
+			{
+				url: '/og-image.jpg',
+				width: 1200,
+				height: 630,
+				alt: 'TheCraftSync Open Graph Image',
+			},
+		],
+		locale: 'en_US',
+		type: 'website',
+	},
+	twitter: {
+		card: 'summary_large_image',
+		title: 'TheCraftSync | Digital Agency',
+		description: 'Digital Agency for all your crafting needs. Make your ideas come to life with our expert crafting services.',
+		images: ['/og-image.jpg'],
+	},
 }
 
 export default function RootLayout({
